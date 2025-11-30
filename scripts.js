183514da323a803ffc291bc6af5e4cbd72b44a2e
wf-mecanica <wfautocenter1996@gmail.com>
2025-11-30 13:02:14 -0300

    Add scroll event listener to change header style

--- scripts.js
+++ scripts.js
@@ -16,3 +16,13 @@ function handleForm(e){
   e.target.reset();
   return false;
 }
+// Encolher logo + mudar fundo do menu ao rolar
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('.site-header');
+
+  if (window.scrollY > 20) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
